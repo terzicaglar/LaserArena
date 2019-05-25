1c3398e93973b9df007921370c2ce72d592b6b8a
terzicaglar <terzicaglar@gmail.com>
2019-05-25 18:41:59 +0300

    Side became abstract class and abstract method action is implemented in Side. All of Side's sublasses implemented overriden action method.

--- src/sides/TargetableSide.java
+++ src/sides/TargetableSide.java
@@ -1,6 +1,11 @@
 package sides;
 
+import core.Direction;
 import interfaces.Targetable;
 
 public class TargetableSide extends Side implements Targetable {
+    @Override
+    public Direction action() {
+        return null;
+    }
 }
