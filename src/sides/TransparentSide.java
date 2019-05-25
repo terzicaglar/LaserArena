5f33a6e8ed07a32a8ec1a6047494805b88f0c502
terzicaglar <terzicaglar@gmail.com>
2019-05-25 18:38:36 +0300

    Side became abstract class and abstract method action is implemented in Side. All of Side's sublasses implemented overriden action method.

--- src/sides/TransparentSide.java
+++ src/sides/TransparentSide.java
@@ -1,6 +1,11 @@
 package sides;
 
+import core.Direction;
 import interfaces.Transparent;
 
 public class TransparentSide extends Side implements Transparent {
+    @Override
+    public Direction action() {
+        return null;
+    }
 }
