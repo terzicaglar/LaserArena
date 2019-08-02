9c73c04d28b461fc1dddee2d876352ed981dbb74
Caglar Terzi <terzicaglar@gmail.com>
2019-08-02 18:51:02 +0300

    LaserBeam graphics (2D Lines) are added to ArenaPanel.

--- src/core/ArenaPanel.java
+++ src/core/ArenaPanel.java
@@ -32,16 +32,73 @@ public class ArenaPanel extends JPanel {
         else
             text = GameMap.getTokenLocatedInXY(x,y).toIconString();
         FontMetrics fm = g2d.getFontMetrics();
-        int beamNo = 0;
+        int beamNo = 0, line_x2 = 0, line_y2 = 0, prev_line_x2 = 0, prev_line_y2 = 0;
+        Direction prevDirection= null;
+        Color colors[] = {Color.RED, Color.BLUE, Color.GREEN, Color.ORANGE, Color.YELLOW, Color.CYAN};
         for(LaserBeam beam: GameMap.getBeams())
         {
+            //TODO: Refactor code below
             for(PointWithDirection pwd: beam.getPathHistory())
             {
                 if(x == (int)pwd.getPoint().getX() && y == (int)pwd.getPoint().getY())
                 {
-                    text += "X" + beamNo + pwd.getDirection().toString().substring(0,2);
-                }
+                    text += " " + beamNo + pwd.getDirection().toString().substring(0,2);
+                    g2d.setColor(colors[beamNo]);
+                    prev_line_x2 = prev_line_y2 = line_y2 = line_x2 = -1;
+                    if(pwd.getDirection().isMovable()) {
 
+                        if(prevDirection == Direction.EAST)
+                        {
+                            prev_line_x2 = 0;
+                            prev_line_y2 = getHeight()/2;
+
+                        }
+                        else if(prevDirection == Direction.WEST)
+                        {
+                            prev_line_x2 = getWidth();
+                            prev_line_y2 = getHeight()/2;
+                        }
+                        else if(prevDirection == Direction.NORTH)
+                        {
+                            prev_line_x2 = getWidth()/2;
+                            prev_line_y2 = getHeight();
+                        }
+                        else if(prevDirection == Direction.SOUTH)
+                        {
+                            prev_line_x2 = getWidth()/2;
+                            prev_line_y2 = 0;
+                        }
+
+
+                        if (pwd.getDirection() == Direction.EAST) {
+                            line_x2 = getWidth();
+                            line_y2 = getHeight() / 2;
+                        }
+                        else if(pwd.getDirection() == Direction.WEST)
+                        {
+                            line_x2 = 0;
+                            line_y2 = getHeight()/2;
+                        }
+                        else if(pwd.getDirection() == Direction.NORTH)
+                        {
+                            line_x2 = getWidth()/2;
+                            line_y2 = 0;
+                        }
+                        else if(pwd.getDirection() == Direction.SOUTH)
+                        {
+                            line_x2 = getWidth()/2;
+                            line_y2 = getHeight();
+                        }
+
+
+                        if(prev_line_x2 != -1 && prev_line_y2 != -1)
+                            g2d.drawLine(getWidth()/2, getHeight()/2, prev_line_x2, prev_line_y2);
+                        if(line_x2 != -1 && line_y2 != -1)
+                            g2d.drawLine(getWidth() / 2, getHeight() / 2, line_x2, line_y2);
+                    }
+                    g2d.setColor(Color.BLACK);
+                }
+                prevDirection = pwd.getDirection();
             }
             beamNo++;
         }
